1e99efdd9691ba99b005b2171e021ea90c67cafe
SanketTambe39 <sankettambe39@gmail.com>
2021-06-26 09:55:07 +0530

    ADDED: Temperature Conversion

--- LogicalPrograms/src/com/bridgelabz/logicalprograms/TemperatureConversion.java
+++ LogicalPrograms/src/com/bridgelabz/logicalprograms/TemperatureConversion.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class TemperatureConversion {
 
 	public static void main(String[] args) {
-		
+		double fahrenheit,celsius;
 		System.out.println("1) fahrenheit to Celsius");
 		System.out.println("2) Celsius to  fahrenheit");
 		System.out.println("3) Exit");
@@ -16,15 +16,15 @@ public class TemperatureConversion {
 		switch (ch) {
 		case 1:
 			System.out.println("Enter Fahrenheit :: ");
-			double f1=scanner.nextDouble();
-			double c1= (f1-32) * 5/9;
-			System.out.println("conversion of given temprature is :: "+c1);
+			fahrenheit = scanner.nextDouble();
+			celsius = (fahrenheit-32) * 5/9;
+			System.out.println("conversion of given temprature is :: "+celsius);
 			break;
 		case 2:
 			System.out.println("Enter Celsius :: ");
-			double c2=scanner.nextDouble();
-			double f2= (c2* 9/5)+ 32;
-			System.out.println("conversion of given temprature is :: "+f2);
+			celsius = scanner.nextDouble();
+			fahrenheit = (celsius* 9/5)+ 32;
+			System.out.println("conversion of given temprature is :: "+fahrenheit);
 			break;
 		case 3:
 			System.exit(0);
